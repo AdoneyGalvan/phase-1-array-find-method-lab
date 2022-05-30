97008208576ddd265a9783f45639d9e1a7c0f66f
Aron Galvan <arongalvan03@gmail.com>
2022-05-30 15:42:11 -0700

    commit tree 0083e23e01cee81c24794fcee89bf216b7ab4663

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const superbowlWin = (record) => {
+
+    let item = record.find((item) => { 
+        return item.result == "W";
+    });
+
+    if (item != undefined)
+    {
+        return item.year;
+    }
+    else{
+        return item;
+    }
+}
+
+
